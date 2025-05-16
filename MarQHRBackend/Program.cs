d7bdaf7cd9edf6832df1f3925e3e388171139eef
Arthur Sornas <sornasarthur93@gmail.com>
2025-05-15 23:22:56 -0300

    Importei as bibliotecas necessárias, criei a classe Colaborador e o construtor da classe Colaborador.

--- MarQHRBackend/Program.cs
+++ MarQHRBackend/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Colaborador
+{
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    public Dictionary<string, decimal> SaldoCategoria { get; set; }
+
+    public Colaborador(string nome, string cpf)
+    {
+        Nome = nome;
+        CPF = cpf;
+        SaldoCategoria = new Dictionary<string, decimal>
+        {
+            {"Alimentação", 1000m},
+            {"Saúde", 1000m},
+            {"Transporte", 1000m},
+            {"Cultura", 1000m},
+            {"Outros", 1000m},
+        };
+    }
+}
